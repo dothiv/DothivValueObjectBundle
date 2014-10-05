32ca3947186da6e4138085b2256271f6766cf272
Markus Tacker <m@coderbyheart.de>
2014-10-05 16:10:50 +0200

    Moved.

--- Tests/autoload.php
+++ Tests/autoload.php
@@ -1,10 +1,10 @@
 <?php
 
 spl_autoload_register(function ($class) {
-    if (strpos($class, 'Dothiv\ValueObject') === 0) {
+    if (strpos($class, 'Dothiv') === 0) {
         $parts = explode('\\', $class);
         array_shift($parts);
-        require_once __DIR__ . '/../' . join(DIRECTORY_SEPARATOR, $parts) . '.php';
+        require_once __DIR__ . '/../Dothiv/' . join(DIRECTORY_SEPARATOR, $parts) . '.php';
         return true;
     }
     return false;
